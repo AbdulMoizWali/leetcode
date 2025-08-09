4497e29852486b894cf12ae9feb1574387c3ef50
AbdulMoiz <85098925+AbdulMoizWali@users.noreply.github.com>
2025-08-09 20:40:53 +0500

    feat: Add solution for 238 Product of Array Except Self problem

---
+++ 238. Product of Array Except Self.py
@@ -0,0 +1,73 @@
+"""
+238. Product of Array Except Self
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+Example 2:
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+
+Constraints:
+
+2 <= nums.length <= 105
+-30 <= nums[i] <= 30
+The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+
+
+Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+"""
+
+"""
+Taken help by Apna College tutorial
+"""
+
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        prefix = [1]
+        suffix = [1]
+        for i in range(1, len(nums)):
+            prefix.append(nums[i - 1] * prefix[i - 1])
+
+        for i in range(len(nums) - 1, 0, -1):
+            suffix.insert(0, nums[i] * suffix[0])
+
+        for i in range(len(nums)):
+            ans.append(prefix[i] * suffix[i])
+
+        print(ans)
+        return ans
+
+
+sol = Solution()
+
+sol.productExceptSelf([1, 2, 3, 4])
+sol.productExceptSelf([-1, 1, 0, -3, 3])
+
+"""
+1 2 3 4 5
+
+(1x2) 3 (4x5)
+
+"""
